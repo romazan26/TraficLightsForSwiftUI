cbdf58b755c6dde287c4b0b0df8511812f2dcaa4
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-07-17 21:55:11 +0300

    Initial Commit

---
+++ TraficLightsForSwiftUI/TraficLightsForSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraficLightsForSwiftUIApp.swift
+//  TraficLightsForSwiftUI
+//
+//  Created by Роман on 17.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TraficLightsForSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
